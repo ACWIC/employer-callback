2c004e3debb7d8e3e83a7999ee851f35153195e8
Chris Gough <christopher.d.gough@gmail.com>
2020-09-27 21:14:39 +1000

    improve docstring on POST callbacks

--- app/api/callbacks.py
+++ app/api/callbacks.py
@@ -19,25 +19,35 @@ def create_callback(inputs: CallbackRequest):
 
     The API validates the callback:
 
-    * The posted payload must confirm with the CallbackRequest schema.
-    * The enrollment_id parameter must be well known to the employer.
+    * The posted *payload* must confirm with the CallbackRequest schema.
+    * The *enrollment_id* parameter must be well known to the employer.
       This is a kind of "correlation id" used to associate messages.
-    * The provided key must be valid for that enrolment_id.
+    * The provided *key* must be valid for that enrolment_id.
       This is a kind of shared-secret, set by the employer
       and shared with the training provider
       at the time the enrolment was sent to them.
+    * The returned object contains a *callback_id*.
+      This is a recipient-assigned identifier,
+      which may be useful in debugging.
+    * The returned object contains a *received" date-time stamp,
+      from the recipient's wall clock.
+      Like callback_id, this may be useful in debugging.
 
     TODO:
 
-    * we probably need to design "message" and "message-type" parameters.
-    * this should probably return a unique callback_id (assigned by the API).
-      Not sure why but we have it, maybe it will be useful to the sender.
-      Maybe also return our wall-clock timestamp.
-    * add "sender's temporal scope" parameter - used by employer
-      to sort in the order of sender's intent.
+    * we probably need to design a "message-type" attribute
+      (to the CallbackRequest, i.e. sent with the POST).
+      This could be used to identify the schema
+      against which the payload could be validated.
+      It could also be used by the recipient
+      to process the data that is sent to them.
     * Document idempotency; If we have this already,
       we respond as though this is the first time we ever saw it.
       Astute repeat senders may notice the wall-clock is a bit odd.
+      Error unless the message-type (when it exists),
+      enrolment_id, payload and tp_sequence match.
+      The service will always return the same values
+      for recipient-assigned fields (callback_id, received).
     '''
     callback_repo = S3CallbackRepo()
     use_case = CreateNewCallback(callback_repo=callback_repo)
