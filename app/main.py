46a3565b5187eef57393b0bcabd33fa235f6cd8d
Ahmed Saad <me@rancho.me>
2020-10-03 00:57:33 +0200

    Ops: Adds env vars to support sub apps under api gateway

--- app/main.py
+++ app/main.py
@@ -3,9 +3,14 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from app.api.callbacks import router as v1_router
 
+API_GATEWAY_STAGE_PREFIX = os.environ.get('STAGE_PREFIX', default='')
+API_GATEWAY_SERVICE_PREFIX = os.environ.get('SERVICE_PREFIX', default='')
+
 app = FastAPI(
     title='Enrolments API',
-    openapi_prefix=os.environ.get('STAGE_PREFIX') or ''
+    root_path=API_GATEWAY_STAGE_PREFIX,
+    openapi_url=API_GATEWAY_SERVICE_PREFIX + "/openapi.json",
+    docs_url=API_GATEWAY_SERVICE_PREFIX + "/docs"
 )
 
 app.add_middleware(
@@ -16,7 +21,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(v1_router)
+app.include_router(v1_router, prefix=API_GATEWAY_SERVICE_PREFIX)
+
 
 if __name__ == '__main__':
     # TODO: could be moved into a separate docs-specific entrypoint as
