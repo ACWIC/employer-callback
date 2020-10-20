39d8d1aad49b64601729acee9edf8e988973e5fb
Baris Sari <sariba@itu.edu.tr>
2020-10-20 14:37:27 +0300

    Increase coverage, remove unused prints

--- app/repositories/callback_repo.py
+++ app/repositories/callback_repo.py
@@ -7,8 +7,8 @@ from app.domain.entities.callback import Callback
 class CallbackRepo(abc.ABC):
     @abc.abstractmethod
     def save_callback(self, callback: dict) -> Callback:
-        pass
+        """"""
 
     @abc.abstractmethod
     def get_callbacks_list(self, enrolment_id: str) -> List[Callback]:
-        pass
+        """"""
--- app/repositories/s3_callback_repo.py
+++ app/repositories/s3_callback_repo.py
@@ -32,14 +32,13 @@ class S3CallbackRepo(CallbackRepo):
         from app.repositories.s3_enrolment_repo import S3EnrolmentRepo
 
         enrolment_repo = S3EnrolmentRepo()
+        # check if enrolment exists, it will raise error if it doesn't
+        enrolment_repo.get_enrolment(enrolment_id)
         with handle_s3_errors():
-            # check if enrolment exists, it will raise error if it doesn't
-            enrolment_repo.get_enrolment(enrolment_id)
             # get callbacks for enrolment id
             callbacks_objects_list = self.s3.list_objects(
                 Bucket=settings.CALLBACK_BUCKET, Prefix="{}/".format(enrolment_id)
             )
-        # print("callbacks_objects_list", callbacks_objects_list)
         callbacks_list = []
         # If there have been 0 callbacks, the list should be empty.
         if "Contents" not in callbacks_objects_list:
@@ -54,5 +53,4 @@ class S3CallbackRepo(CallbackRepo):
                 callbacks_list.append(
                     {"callback_id": callback.callback_id, "received": callback.received}
                 )
-        # print("callbacks_list:", [callbacks_list])
         return {"callbacks_list": callbacks_list}
