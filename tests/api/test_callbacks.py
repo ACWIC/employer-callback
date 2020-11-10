deff163a390b31b96c2db97144df89fdc8bb4c56
Baris Sari <sariba@itu.edu.tr>
2020-11-10 17:30:54 +0300

    Cover all endpoint lines

--- tests/api/test_callbacks.py
+++ tests/api/test_callbacks.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 
 from app.domain.entities.callback import Callback
 from app.main import app
-from app.responses import ResponseSuccess, SuccessType
+from app.responses import FailureType, ResponseFailure, ResponseSuccess, SuccessType
 from tests.test_data.callback_provider import CallbackDataProvider
 
 test_data = CallbackDataProvider()
@@ -12,7 +12,7 @@ client = TestClient(app)
 
 
 @mock.patch("app.use_cases.create_new_callback.CreateNewCallback")
-def test_create_callbacks(use_case):
+def test_create_callbacks_success_created(use_case):
     code = SuccessType.CREATED
     message = "The callback has been saved."
     use_case().execute.return_value = ResponseSuccess(
@@ -29,3 +29,37 @@ def test_create_callbacks(use_case):
     use_case().execute.assert_called_with(data)
     assert response.status_code == SuccessType.CREATED.value
     assert callback == test_data.sample_callback
+    assert json_result.get("message") == message
+
+
+@mock.patch("app.use_cases.create_new_callback.CreateNewCallback")
+def test_create_callbacks_success(use_case):
+    message = "The callback has been fetched from the server."
+    use_case().execute.return_value = ResponseSuccess(
+        value=test_data.sample_callback,
+        message=message,
+    )
+
+    data = test_data.sample_callback_request
+    response = client.post("/callbacks", data=data.json())
+    json_result = response.json()
+    callback = Callback(**json_result.get("value"))
+
+    use_case().execute.assert_called_with(data)
+    assert response.status_code == SuccessType.SUCCESS.value
+    assert callback == test_data.sample_callback
+    assert json_result.get("message") == message
+
+
+@mock.patch("app.use_cases.create_new_callback.CreateNewCallback")
+def test_create_callbacks_failure(use_case):
+    message = "Error"
+    use_case().execute.return_value = ResponseFailure.build_from_resource_error(
+        message=message,
+    )
+
+    data = test_data.sample_callback_request
+    response = client.post("/callbacks", data=data.json())
+
+    assert response.status_code == FailureType.RESOURCE_ERROR.value
+    assert response.json() == {"detail": message}
